576b8d1fcfdddacdd959424f4c244322b53e953d
Brian Heck <bheck76@gmail.com>
2026-05-20 09:31:34 -0500

    Add zone performance gold mart

---
+++ sql/gold/create_mart_zone_performance.sql
@@ -0,0 +1,26 @@
+CREATE OR REPLACE TABLE `urban_mobility_gold.mart_zone_performance`
+AS
+
+SELECT
+    pickup_borough,
+    pickup_zone_name,
+
+    COUNT(*) AS total_trips,
+
+    SUM(total_amount) AS total_revenue,
+
+    AVG(total_amount) AS avg_total_fare,
+
+    AVG(trip_distance) AS avg_trip_distance,
+
+    AVG(trip_duration_minutes) AS avg_trip_duration_minutes,
+
+    AVG(fare_per_mile) AS avg_fare_per_mile,
+
+    AVG(tip_percentage) AS avg_tip_percentage
+
+FROM `urban_mobility_gold.fact_taxi_trips`
+
+GROUP BY
+    pickup_borough,
+    pickup_zone_name;
