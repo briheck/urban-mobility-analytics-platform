1d54c58c46cd4361b7359fbd211391c256b0dcee
Brian Heck <bheck76@gmail.com>
2026-05-20 09:29:11 -0500

    Add hourly demand gold mart

---
+++ sql/gold/create_mart_hourly_demand.sql
@@ -0,0 +1,20 @@
+CREATE OR REPLACE TABLE `urban_mobility_gold.mart_hourly_demand`
+AS
+
+SELECT
+    pickup_date,
+    pickup_hour,
+    pickup_borough,
+
+    COUNT(*) AS total_trips,
+    SUM(total_amount) AS total_revenue,
+    AVG(total_amount) AS avg_total_fare,
+    AVG(trip_distance) AS avg_trip_distance,
+    AVG(trip_duration_minutes) AS avg_trip_duration_minutes
+
+FROM `urban_mobility_gold.fact_taxi_trips`
+
+GROUP BY
+    pickup_date,
+    pickup_hour,
+    pickup_borough;
